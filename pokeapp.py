0f81281acd328a789040ac00e7eabddcd5a7236b
Oliver Thejl Eriksen <olivereriksen@protonmail.com>
2019-12-22 20:18:48 +0100

    Exported pokeapp.txt

---
+++ pokeapp.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Oliver Thejl Eriksen
+#
+# Created:     22-12-2019
+# Copyright:   (c) Oliver Thejl Eriksen 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+
+def main():
+    5
+
+if __name__ == '__main__':
+    main()
